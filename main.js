e5efc8391d4adc98758f1fadb1844e0a70099b9e
sathishkumar330 <svksathish41@gmail.com>
2023-04-04 14:53:44 +0530

    add

---
+++ main.js
@@ -0,0 +1,15 @@
+var a=prompt("Enter the age");
+var b=prompt("Enter the age");
+var c=prompt("Enter the age");
+
+if((a>b)&&(a>c)){
+	console.log("A is older");
+}
+
+else if((b>c)&&(b>a)){
+	console.log("B is older");
+}
+
+else{
+	console.log("C is older");
+}
